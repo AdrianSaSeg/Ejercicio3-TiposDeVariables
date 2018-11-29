cf1727a60fb01ae6131df9cc36a3ce7796cb9573
AdrianSanchezSegura <murciadri@gmail.com>
2018-11-29 09:59:24 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3_TiposVariablesEstaticas/Variables.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio3_TiposVariablesEstaticas
+{
+    class Variables
+    {
+        public static string nombre;
+        internal static int edad;
+        private static bool disponible;
+        protected static DateTime fechaAlta;
+
+    }
+}
