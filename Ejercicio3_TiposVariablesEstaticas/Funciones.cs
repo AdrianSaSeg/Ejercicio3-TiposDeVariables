5f63c685e6588ee0d453272748dba05b6c01cd50
AdrianSanchezSegura <murciadri@gmail.com>
2018-11-29 11:44:57 +0100

    Cambio

---
+++ Ejercicio3_TiposVariablesEstaticas/Funciones.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio1_FuncionesBasic
+{
+    class Funciones
+    {
+        public static int suma (int x, int y)
+        {
+            return x + y;
+        }
+
+        public static double calculaIRPF(double sueldoBruto, double porcentaje)
+        {
+            return (sueldoBruto * porcentaje) / 100;
+        }
+
+        public static Boolean eliminarUsuario(int id, string nombre)
+        {
+            return true;
+        }
+
+        public static void imprimirTexto(string texto)
+        {
+
+        }
+
+        public static void insertarNoticia(int id_noticia, DateTime fecha)
+        {
+
+        }
+        
+        public static int CambioValor(ref int a, int b)
+        {
+            a = 1;
+            return b;
+        }
+    }
+}
